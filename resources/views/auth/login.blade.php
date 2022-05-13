c8141926f82dbb8dd44ba2e93b3047c3789f03d8
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-05-12 22:36:25 -0400

    login dashmix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,95 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="bg-image" style="background-image: url('/media/photos/photo22@2x.jpg');">
+        <div class="row g-0 bg-primary-op">
+            <!-- Main Section -->
+            <div class="hero-static col-md-6 d-flex align-items-center bg-body-extra-light">
+                <div class="p-3 w-100">
+                    <!-- Header -->
+                    <div class="mb-3 text-center">
+                        <a class="link-fx fw-bold fs-1" href="index.html">
+                            <span class="text-dark">Dash</span><span class="text-primary">mix</span>
+                        </a>
+                        <p class="text-uppercase fw-bold fs-sm text-muted">Sign In</p>
+                    </div>
+                    <!-- END Header -->
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                    <!-- Sign In Form -->
+                    <!-- jQuery Validation (.js-validation-signin class is initialized in js/pages/op_auth_signin.min.js which was auto compiled from _js/pages/op_auth_signin.js) -->
+                    <!-- For more info and examples you can check out https://github.com/jzaefferer/jquery-validation -->
+                    <div class="row g-0 justify-content-center">
+                        <div class="col-sm-8 col-xl-6">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                            <form class="js-validation-signin" method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="py-3">
+                                    <div class="mb-4">
+                                        <input id="email" type="email"
+                                            class="form-control form-control-lg form-control-alt  @error('email') is-invalid @enderror"
+                                            name="email" placeholder="Email" value="{{ old('email') }}" required
+                                            autocomplete="email" autofocus>
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <div class="mb-4">
+                                        <input id="password" type="password"
+                                            class="form-control form-control-lg form-control-alt  @error('password') is-invalid @enderror"
+                                            name="password" placeholder="Password" required autocomplete="current-password">
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                                <div class="mb-4">
+                                    <button type="submit" class="btn w-100 btn-lg btn-hero btn-primary">
+                                        <i class="fa fa-fw fa-sign-in-alt opacity-50 me-1"></i> Sign In
+                                    </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+
+                                    <p class="mt-3 mb-0 d-lg-flex justify-content-lg-between">
+                                        @if (Route::has('password.request'))
+                                            <a class="btn btn-sm btn-alt-secondary d-block d-lg-inline-block mb-1"
+                                                href="{{ route('password.request') }}">
+                                                <i class="fa fa-exclamation-triangle opacity-50 me-1"></i> Forgot password
+                                            </a>
+                                        @endif
+                                        @if (Route::has('register'))
+                                            <a class="btn btn-sm btn-alt-secondary d-block d-lg-inline-block mb-1"
+                                                href="{{ route('register') }}">
+                                                <i class="fa fa-plus opacity-50 me-1"></i> New Account
+                                            </a>
+                                        @endif
+
+                                    </p>
+                                </div>
+                            </form>
                         </div>
-                    </form>
+                    </div>
+                    <!-- END Sign In Form -->
                 </div>
             </div>
+            <!-- END Main Section -->
+
+            <!-- Meta Info Section -->
+            <div
+                class="hero-static col-md-6 d-none d-md-flex align-items-md-center justify-content-md-center text-md-center">
+                <div class="p-3">
+                    <p class="display-4 fw-bold text-white mb-3">
+                        Welcome to the future
+                    </p>
+                    <p class="fs-lg fw-semibold text-white-75 mb-0">
+                        Copyright &copy; <span data-toggle="year-copy"></span>
+                    </p>
+                </div>
+            </div>
+            <!-- END Meta Info Section -->
         </div>
     </div>
-</div>
 @endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Icons -->
+    <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
+    <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('media/favicons/favicon-192x192.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
+
+    <!-- Fonts -->
+    <link rel="stylesheet" id="css-main" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Kori @yield('title')</title>
+
+
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" id="css-main" href="{{ asset('css/dashmix.css') }}">
+    <link rel="stylesheet" id="css-theme" href="{{ asset('css/dashmix.css') }}">
+    <link rel="stylesheet" id="css-theme" href="{{ asset('css/stepper_style.css') }}">
+    <link rel="stylesheet" id="css-theme" href="{{ asset('css/map_style.css') }}">
+
+</head>
+<body>
+    <main>
+        @yield('content')
+    </main>
+
+    @yield('js')
+
+    <!-- Jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <!-- Dashmix Core JS -->
+    <script src="{{ asset('js/dashmix.app.min.js') }}"></script>
+    <script src="{{ asset('js/dashmix.core.min.js') }}"></script>
+
+</body>
+</html>
