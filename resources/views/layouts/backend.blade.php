02ec242ef4b5ff9dcc4efae9f8176d3795378ff4
fvalentin1 <71657263+fvalentin1@users.noreply.github.com>
2022-05-18 02:02:01 -0400

    rutas correccion

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -383,15 +383,13 @@
                                     </a>
                                 </li>
                                 <li class="nav-main-item">
-                                    <a class="nav-main-link{{ request()->is('pages/slick') ? ' active' : '' }}"
-                                        href="/pages/slick">
-                                    <a class="nav-main-link{{ request()->is('/characteristics/index_category') ? ' active' : '' }}"
+                                    <a class="nav-main-link{{ request()->is('characteristics/index_category') ? ' active' : '' }}"
                                         href="{{ route('list_category') }}">
                                         <span class="nav-main-link-name">Categorías</span>
                                     </a>
                                 </li>
                                 <li class="nav-main-item">
-                                    <a class="nav-main-link{{ request()->is('characteristics/list_provider') ? ' active' : '' }}"
+                                    <a class="nav-main-link{{ request()->is('characteristics/index_provider') ? ' active' : '' }}"
                                         href="{{ route('list_provider') }}">
                                         <span class="nav-main-link-name">Proveedores</span>
                                     </a>
