4cc526cefa4def8b6ba12eb20f47fdb12e410315
fvalentin1 <71657263+fvalentin1@users.noreply.github.com>
2022-05-28 13:25:14 -0400

    pantalla welcome
    se eliminaron referencias de laravel

--- resources/views/public/welcome.blade.php
+++ resources/views/public/welcome.blade.php
@@ -55,52 +55,56 @@
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
                             <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Producto 1</a></div>
+                                <span style="font-size: 220%"><i class="fa-brands fa-instagram"></i></span>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://www.instagram.com/korimangastore/" class="underline text-gray-900 dark:text-white">Instagram</a></div>
                             </div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                    Siguenos en nuestro perfil en Instagram, enterate de novedades y participa de concursos.
                                 </div>
                             </div>
                         </div>
 
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                             <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">Producto 2</a></div>
+                                <span style="font-size: 220%"><i class="fa-brands fa-facebook"></i></span>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://www.facebook.com/Kori-Manga-Store-108678028046259" class="underline text-gray-900 dark:text-white">Facebook</a></div>
                             </div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+                                    Visita nuestra Fan Page en Facebook, enterate de novedades, noticias, artículos y reacciona a nuestras publicaciones.
                                 </div>
                             </div>
                         </div>
 
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700">
                             <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel-news.com/" class="underline text-gray-900 dark:text-white">Producto 3</a></div>
+                                <span style="font-size: 220%"><i class="fa-solid fa-location-dot"></i></span>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://www.google.com/maps/@-36.8264254,-73.0528476,20z" class="underline text-gray-900 dark:text-white">Tienda Física</a></div>
                             </div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel News is a community driven portal and newsletter aggregating all of the latest and most important news in the Laravel ecosystem, including new package releases and tutorials.
+                                    Nos encontramos en Freire #522, Galería Caracol, 4to piso, Local 185. Concepción.
+                                    <br>
+                                    Horario de atención: Lunes a Viernes de 11:00 am - 19:00 pm.
                                 </div>
                             </div>
                         </div>
 
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
                             <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Producto 4</div>
+                                <span style="font-size: 220%"><i class="fa-solid fa-book"></i></span>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://docs.google.com/spreadsheets/d/1kugVxHfw2fqV2WfjXpl8Ejt9hO0GGm02qgLUAi7TEuE/edit#gid=0" class="underline text-gray-900 dark:text-white">Revisa nuestro catálogo</a></div>
                             </div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel's robust library of first-party tools and libraries, such as <a href="https://forge.laravel.com" class="underline">Forge</a>, <a href="https://vapor.laravel.com" class="underline">Vapor</a>, <a href="https://nova.laravel.com" class="underline">Nova</a>, and <a href="https://envoyer.io" class="underline">Envoyer</a> help you take your projects to the next level. Pair them with powerful open source libraries like <a href="https://laravel.com/docs/billing" class="underline">Cashier</a>, <a href="https://laravel.com/docs/dusk" class="underline">Dusk</a>, <a href="https://laravel.com/docs/broadcasting" class="underline">Echo</a>, <a href="https://laravel.com/docs/horizon" class="underline">Horizon</a>, <a href="https://laravel.com/docs/sanctum" class="underline">Sanctum</a>, <a href="https://laravel.com/docs/telescope" class="underline">Telescope</a>, and more.
+                                    Revisa el stock de nuestros productos.
+                                    <br>
+                                    Pronto tendremos para tí nuestra Tienda Virtual, donde podrás revisar nuestro catálogo, con fotos, descripción, y promociones de nuestros productos, además de envíos a todo Chile.
                                 </div>
                             </div>
                         </div>
@@ -117,6 +121,7 @@
                 </div>
             </div>
         </div>
+        <script src="https://kit.fontawesome.com/207ceca2f0.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
