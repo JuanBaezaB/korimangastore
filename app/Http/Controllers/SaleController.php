dd1c8e8c1ec86c845a4bb16a73a543542cce2ef4
Diego Romero Carrillo <dromero@ing.ucsc.cl>
2022-06-23 15:55:49 -0400

    Agregación estadísticas

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -32,7 +32,6 @@ class SaleController extends Controller
 
         $the_compact = compact('branches', 'is_all_branches', 'the_branch');
         return response()->view('admin.sales.list_sale', $the_compact);
-
     }
 
     /**
@@ -62,6 +61,7 @@ class SaleController extends Controller
     public function store(Request $request)
     {
         //
+<<<<<<< Updated upstream
         $quantity = $request->get('quantity');
 
         $normalizedQuantity = ($request->get('reverse')) ? -$quantity : $quantity;
@@ -89,6 +89,45 @@ class SaleController extends Controller
             'product' => $product
         ];
         return response()->json($ret);
+=======
+        $request->validate([
+            'products' => 'array|required',
+            'products.*.id' => 'exists:products,id|required',
+            'products.*.n' => 'gt:0|integer|required',
+            'discount_amount' => 'gte:0|integer',
+            'branch_id' => 'exists:branches,id|required'
+        ]);
+
+        $all = collect($request->all());
+        $discountAmount = $all->get('discount_amount');
+        $productsRaw = collect($all->get('products'));
+        $branch_id = $all->get('branch_id');
+        $branch = Branch::find($branch_id);
+        $products = $productsRaw->mapWithKeys(function ($item, $key) {
+            return [
+                $item['id'] => [
+                    'amount' => $item['n']
+                ]
+            ];
+        });
+
+        DB::transaction(function ()
+        use ($products, $branch, $discountAmount, $request, $branch_id) {
+            foreach ($products as $id => $stuff) {
+                // TODO: cambiar este hack
+                (new StockController)->change(-$stuff['amount'], $branch, $id);
+            }
+
+            $sale = new Sale();
+            $sale->discount_amount = $discountAmount;
+            $sale->user_id = $request->user()->id;
+            $sale->branch_id = $branch_id;
+            $sale->save();
+
+            $sale->products()->sync($products);
+        });
+        return response()->json(['success' => true]);
+>>>>>>> Stashed changes
     }
 
     /**
@@ -136,10 +175,12 @@ class SaleController extends Controller
         //
     }
 
-    public function list(Request $request) {
+    public function list(Request $request)
+    {
         $givenId = $request->get('id', null);
 
         if (!$givenId) {
+<<<<<<< Updated upstream
             $products = Sale::with('branches', 'category')
             ->withSum('branches', 'branch_product.stock')
             ->get()
@@ -156,90 +197,100 @@ class SaleController extends Controller
             ->toArray();
         }
         return response()->json([ "data" => $products]);
+=======
+            $sales = Sale::with(['branch', 'products', 'user'])
+                ->withCount('products')
+                ->addSelect([
+                    'total_price' => Product::selectRaw('SUM(product_sale.amount * products.price)')
+                        ->join('product_sale', 'product_sale.product_id', '=', 'products.id')
+                        ->whereColumn('product_sale.sale_id', 'sales.id')
+                ]);
+        } else {
+            $sales = Sale::with(['branch', 'products', 'user'])
+                ->withCount('products')
+                ->whereRelation('branch', 'id', $givenId)
+                ->addSelect([
+                    'total_price' => Product::selectRaw('SUM(product_sale.amount * products.price)')
+                        ->join('product_sale', 'product_sale.product_id', '=', 'products.id')
+                        ->whereColumn('product_sale.sale_id', 'sales.id')
+                ]);
+        }
+        $sales = $sales->get()->toArray();
+        return response()->json(["data" => $sales]);
+>>>>>>> Stashed changes
     }
-        
+
 
 
     public function charts()
     {
-        /*
-        $mes = substr($hoy, 0, 7);
-        $ventames = DB::table('ventas')->where('created_at', '>=', $mes . '-01')->where('created_at', '<=', $mes . '-31')->sum('valor');
-
-        $year = substr($hoy, 0, 4);
-        $ventanual = DB::table('ventas')->where('created_at', '>=', $year . '-01-01')->where('created_at', '<=', $year . '-12-31')->sum('valor');
-*/
-
+        $salesMonth = [];
+        $percentListSales = [];
+        $percentListUser = [];
 
         $sales = Sale::all();
         $cUsers  = User::count();
         $cSales = Sale::count();
         $cProducts = Product::count();
 
-
-        //Ganancias y porcentaje
         $dateFrom = Carbon::now()->subDays(30);
         $dateTo = Carbon::now();
 
         $previousDateFrom = Carbon::now()->subDays(60);
         $previousDateTo = Carbon::now()->subDays(31);
-        $previousMonthly = Sale::where('created_at', '>=', $dateFrom)->where('created_at', '<=', $dateTo)->count();
 
-        //Ultimos 30 usuarios
-        /*
-        $lastThirtyUsers = User::select('id')
-            ->where('name', 'user')
-            ->where('created_at', '>', now()->subDays(30)->endOfDay())
-            ->all();
-            */
-        $salesMonth = [];
-        
-            $salesMonth['Enero'] =  Sale::whereMonth('created_at', '=', '01')->whereYear('created_at', '=', date("Y"))->count('id');
-            $salesMonth['Febrero'] =  Sale::whereMonth('created_at', '=', '02')->whereYear('created_at', '=', date("Y"))->count('id');
-            $salesMonth['Marzo'] =  Sale::whereMonth('created_at', '=', '03')->whereYear('created_at', '=', date("Y"))->count('id');
-            $salesMonth['Abril'] =  Sale::whereMonth('created_at', '=', '04')->whereYear('created_at', '=', date("Y"))->count('id');
-            $salesMonth['Mayo'] =  Sale::whereMonth('created_at', '=', '05')->whereYear('created_at', '=', date("Y"))->count('id');
-            $salesMonth['Junio'] =  Sale::whereMonth('created_at', '=', '06')->whereYear('created_at', '=', date("Y"))->count('id');
-            $salesMonth['Julio'] =  Sale::whereMonth('created_at', '=', '07')->whereYear('created_at', '=', date("Y"))->count('id');
-            $salesMonth['Agosto'] =  Sale::whereMonth('created_at', '=', '08')->whereYear('created_at', '=', date("Y"))->count('id');
-            $salesMonth['Septiembre'] =  Sale::whereMonth('created_at', '=', '09')->whereYear('created_at', '=', date("Y"))->count('id');
-            $salesMonth['Octubre'] =  Sale::whereMonth('created_at', '=', '10')->whereYear('created_at', '=', date("Y"))->count('id');
-            $salesMonth['Noviembre'] =  Sale::whereMonth('created_at', '=', '11')->whereYear('created_at', '=', date("Y"))->count('id');
-            $salesMonth['Diciembre'] =  Sale::whereMonth('created_at', '=', '12')->whereYear('created_at', '=', date("Y"))->count('id');
+        //Variables function retPercent
 
-            $monthlySales = Sale::where('created_at', '>=', $dateFrom)->where('created_at', '<=', $dateTo)->count();
-            $monthlyUsers = User::where('created_at', '>=', $dateFrom)->where('created_at', '<=', $dateTo)->count();
+        $monthlySales = Sale::where('created_at', '>=', $dateFrom)->where('created_at', '<=', $dateTo)->count();
+        $previousMonthlySales = Sale::where('created_at', '>=', $previousDateFrom)->where('created_at', '<=', $previousDateTo)->count();
 
-        
+        $monthlyUsers = User::where('created_at', '>=', $dateFrom)->where('created_at', '<=', $dateTo)->count();
+        $previousMonthlyUser = User::where('created_at', '>=', $previousDateFrom)->where('created_at', '<=', $previousDateTo)->count();
 
-           // $percentSales = devuelvePorcentaje($monthlySales);
-            //$percentUsers = devuelvePorcentaje($monthlyUsers);
 
-        
-        return view('admin.basic_management.internal_configuration.sale.graphic.graphic', compact('salesMonth', 'cUsers', 'cSales', 'cProducts', 'sales'));
-    }
 
-    /*
-    public function porcentajeGanancia($monthly)//necesito que reciba una clase (sale, user)
-    {
-        $dateFrom = Carbon::now()->subDays(30);
-        $dateTo = Carbon::now();
 
-        if ($previousMonthly < $monthly) {
-            if ($previousMonthly > 0) {
-                $rest = $monthly - $previousMonthly;
-                $percent = $rest / $previousMonthly * 100; //incremento de porcentaje
-                $mark   ="+";
+        $salesMonth['Enero'] =  Sale::whereMonth('created_at', '=', '01')->whereYear('created_at', '=', date("Y"))->count('id');
+        $salesMonth['Febrero'] =  Sale::whereMonth('created_at', '=', '02')->whereYear('created_at', '=', date("Y"))->count('id');
+        $salesMonth['Marzo'] =  Sale::whereMonth('created_at', '=', '03')->whereYear('created_at', '=', date("Y"))->count('id');
+        $salesMonth['Abril'] =  Sale::whereMonth('created_at', '=', '04')->whereYear('created_at', '=', date("Y"))->count('id');
+        $salesMonth['Mayo'] =  Sale::whereMonth('created_at', '=', '05')->whereYear('created_at', '=', date("Y"))->count('id');
+        $salesMonth['Junio'] =  Sale::whereMonth('created_at', '=', '06')->whereYear('created_at', '=', date("Y"))->count('id');
+        $salesMonth['Julio'] =  Sale::whereMonth('created_at', '=', '07')->whereYear('created_at', '=', date("Y"))->count('id');
+        $salesMonth['Agosto'] =  Sale::whereMonth('created_at', '=', '08')->whereYear('created_at', '=', date("Y"))->count('id');
+        $salesMonth['Septiembre'] =  Sale::whereMonth('created_at', '=', '09')->whereYear('created_at', '=', date("Y"))->count('id');
+        $salesMonth['Octubre'] =  Sale::whereMonth('created_at', '=', '10')->whereYear('created_at', '=', date("Y"))->count('id');
+        $salesMonth['Noviembre'] =  Sale::whereMonth('created_at', '=', '11')->whereYear('created_at', '=', date("Y"))->count('id');
+        $salesMonth['Diciembre'] =  Sale::whereMonth('created_at', '=', '12')->whereYear('created_at', '=', date("Y"))->count('id');
+
+
+        function retPercent($monthly, $previousMonthly) //necesito que reciba una clase (sale, user)
+        {
+
+            $list_data = [];
+            if ($previousMonthly < $monthly) {
+                if ($previousMonthly > 0) {
+                    $sustract = $monthly - $previousMonthly;
+                    $list_data['mark']  = "+";
+                    $list_data['percent'] = $sustract / $previousMonthly * 100; //incremento de porcentaje
+                } else {
+                    $list_data['mark']  = "+";
+                    $list_data['percent'] = 100; //incremento de porcentaje
+
+                }
             } else {
-                $percent = 100; //incremento de porcentaje
+                $sustract = $previousMonthly - $monthly;
+                $list_data['mark'] = "-";
+                $list_data['percent'] = $sustract / $previousMonthly * 100; //disminucion de porcentaje
             }
-        } else {
-            $mark="-";
-            $rest = $previousMonthly - $monthly;
-            $percent = $rest / $previousMonthly * 100; //disminucion de porcentaje
+            return $list_data;
         }
+        $percentListSales = retPercent($monthlySales, $previousMonthlySales);
+        $percentListUsers = retPercent($monthlyUsers, $previousMonthlyUser);
 
-        return 
+
+
+
+        return view('admin.basic_management.internal_configuration.sale.graphic.graphic', compact('salesMonth', 'cUsers', 'cSales', 'cProducts', 'sales', 'percentListUsers', 'percentListSales'));
     }
-*/    
 }
