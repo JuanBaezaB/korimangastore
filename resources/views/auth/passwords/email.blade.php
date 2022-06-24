d8e36a5eb37ae4af1f69b1d5dd3fb03d55b2363c
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-06-23 23:00:56 -0400

    Restablecer contraseña

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,55 +1,57 @@
 @extends('layouts.master')
 @section('content')
+    <!-- Page Content -->
+    <div class="bg-image" style="background-image: url('/media/login/fondo2.jpg');">
+        <div class="row g-0 justify-content-center bg-black-75">
+            <div class="hero-static col-sm-8 col-md-6 col-xl-4 d-flex align-items-center p-2 px-sm-0">
+                <!-- Reminder Block -->
+                <div class="block block-transparent block-rounded w-100 mb-0 overflow-hidden">
+                    <div class="block-content block-content-full px-lg-5 px-xl-6 py-4 py-md-5 py-lg-6 bg-body-extra-light">
+                        <!-- Header -->
+                        <div class="mb-2 text-center">
+                            <a class="link-fx fw-bold fs-1" href="index.php">
+                                <div class="logokori">
+                                    <img src="/media/login/logokori.png" alt="" width="20%" height="20%">
+                                </div>
+                                <span class="text-dark">Kori</span><span class="text-primary">Mangastore</span>
+                            </a>
+                            <p class="text-uppercase fw-bold fs-sm text-muted">Restablecer contraseña</p>
+                        </div>
+                        <!-- END Header -->
 
-<!-- Page Content -->
-<div class="bg-image" style="background-image: url('/media/login/fondo2.jpg');">
-    <div class="row g-0 justify-content-center bg-black-75">
-        <div class="hero-static col-sm-8 col-md-6 col-xl-4 d-flex align-items-center p-2 px-sm-0">
-            <!-- Reminder Block -->
-            <div class="block block-transparent block-rounded w-100 mb-0 overflow-hidden">
-                <div class="block-content block-content-full px-lg-5 px-xl-6 py-4 py-md-5 py-lg-6 bg-body-extra-light">
-                    <!-- Header -->
-                    <div class="mb-2 text-center">
-                        <a class="link-fx fw-bold fs-1" href="index.php">
-                        <div class="logokori">
-                                <img src="/media/login/logokori.png" alt="" width="20%" height="20%">
+                        <form method="POST" action="{{ route('password.email') }}">
+                            @csrf
+                            <div class="mb-4">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-text">
+
+                                        <i class="fa fa-user-circle"></i>
+                                    </span>
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email" autofocus
+                                        placeholder="Dirección de correo electrónico">
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                            <span class="text-dark">Kori</span><span class="text-primary">Mangastore</span>
-                        </a>
-                        <p class="text-uppercase fw-bold fs-sm text-muted">Restablecer contraseña</p>
+                            <div class="text-center mb-4">
+                                <button type="submit" class="btn btn-hero btn-primary">
+                                    <i class="fa fa-fw fa-reply opacity-50 me-1"></i>
+                                    {{ __('Enviar enlace de restablecimiento de contraseña') }}
+                                </button>
+                            </div>
+
+                        </form>
+                        <!-- END Reminder Form -->
                     </div>
-                    <!-- END Header -->
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-                        <div class="mb-4">
-                            <div class="input-group input-group-lg">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror<span class="input-group-text">
-
-                                    <i class="fa fa-user-circle"></i>
-                                </span>
-                            </div>
-                        </div>
-                        <div class="text-center mb-4">
-                            <button type="submit" class="btn btn-hero btn-primary">
-                                <i class="fa fa-fw fa-reply opacity-50 me-1"></i> {{ __('Enviar enlace de restablecimiento de contraseña') }}
-                            </button>
-                        </div>
-                        
-                    </form>
-                    <!-- END Reminder Form -->
                 </div>
+                <!-- END Reminder Block -->
             </div>
-            <!-- END Reminder Block -->
         </div>
     </div>
-</div>
-<!-- END Page Content -->
+    <!-- END Page Content -->
 @endsection
-
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,23 +1,53 @@
 @extends('layouts.master')
+
+
 @section('content')
-    <div class="container"><br><br><br>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Restablecer Contraseña') }}</div>
-                    <div class="card-body">
+    <!-- Page Content -->
+    <div class="bg-image" style="background-image: url('/media/login/fondo2.jpg');">
+        <div class="row g-0 justify-content-center bg-black-75">
+            <div class="hero-static col-sm-8 col-md-6 col-xl-4 d-flex align-items-center p-2 px-sm-0">
+                <!-- Reminder Block -->
+                <div class="block block-transparent block-rounded w-100 mb-0 overflow-hidden">
+                    <div class="block-content block-content-full px-lg-5 px-xl-6 py-4 py-md-5 py-lg-6 bg-body-extra-light">
+                        <!-- Header -->
+                        <div class="mb-2 text-center">
+                            <a class="link-fx fw-bold fs-1" href="index.php">
+                                <div class="logokori">
+                                    <img src="/media/login/logokori.png" alt="" width="20%" height="20%">
+                                </div>
+                                <span class="text-dark">Kori</span><span class="text-primary">Mangastore</span>
+                            </a>
+                            <p class="text-uppercase fw-bold fs-sm text-muted">Restablecer contraseña</p>
+                        </div>
+                        <!-- END Header -->
+
                         <form method="POST" action="{{ route('password.update') }}">
                             @csrf
                             <input type="hidden" name="token" value="{{ $token }}">
-
-                            <div class="row mb-3">
-                                <label for="password"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Contraseña') }}</label>
-
-                                <div class="col-md-6">
+                            <div class="mb-4">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-text">
+                                        <i class="fa fa-user-circle"></i>
+                                    </span>
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus
+                                        placeholder="Dirección de correo electrónico">
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="mb-4">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-text">
+                                        <i class="fa fa-asterisk"></i>
+                                    </span>
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="new-password">
+                                        required autocomplete="new-password" placeholder="Contraseña">
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -25,25 +55,28 @@
                                     @enderror
                                 </div>
                             </div>
-                            <div class="row mb-3">
-                                <label for="password-confirm"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Confirmar contraseña') }}</label>
-                                <div class="col-md-6">
+                            <div class="mb-4">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-text">
+                                        <i class="fa fa-asterisk"></i>
+                                    </span>
                                     <input id="password-confirm" type="password" class="form-control"
-                                        name="password_confirmation" required autocomplete="new-password">
+                                        name="password_confirmation" required autocomplete="new-password"
+                                        placeholder="Confirmar Contraseña">
                                 </div>
                             </div>
-                            <div class="row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Restablecer Contraseña') }}
-                                    </button>
-                                </div>
+                            <div class="text-center mb-4">
+                                <button type="submit" class="btn btn-hero btn-primary">
+                                    {{ __('Restablecer Contraseña') }}
+                                </button>
                             </div>
                         </form>
+                        <!-- END Reminder Form -->
                     </div>
                 </div>
+                <!-- END Reminder Block -->
             </div>
         </div>
     </div>
+    <!-- END Page Content -->
 @endsection
