0bc341286a5c1f2e4d2c1d01c653594633e4376b
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-07-08 22:01:17 -0400

    datatable responsive

--- resources/views/admin/product_management/list_product.blade.php
+++ resources/views/admin/product_management/list_product.blade.php
@@ -112,95 +112,11 @@
     <script src="{{ asset('js/plugins/datatables-buttons/buttons.html5.min.js') }}"></script>
     <script src="{{ asset('js/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
     <script src="{{ asset('js/plugins/datatables-responsive/js/responsive.bootstrap5.js') }}"></script>
-
     <script>
-        jQuery.extend(jQuery.fn.dataTable.ext.classes, {
-            sWrapper: "dataTables_wrapper dt-bootstrap5",
-            sFilterInput: "form-control",
-            sLengthSelect: "form-select"
-        });
-
-        // Override a few defaults
-        jQuery.extend(true, jQuery.fn.dataTable.defaults, {
-            language: {
-                lengthMenu: "_MENU_",
-                search: "_INPUT_",
-                searchPlaceholder: "Buscar...",
-                info: "Pagina <strong>_PAGE_</strong> de <strong>_PAGES_</strong>",
-                paginate: {
-                    first: '<i class="fa fa-angle-double-left"></i>',
-                    previous: '<i class="fa fa-angle-left"></i>',
-                    next: '<i class="fa fa-angle-right"></i>',
-                    last: '<i class="fa fa-angle-double-right"></i>'
-                }
-            }
-        });
-
-        jQuery('#product-table').DataTable({
-            dom: '<"row"<"col-sm-12 col-md-6"<"dt-buttons btn-group flex-wrap"B>><"col-sm-12 col-md-6"f>>t<"row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
-            responsive: true,
-            columnDefs: [{
-                    responsivePriority: 1,
-                    targets: 0
-                },
-                {
-                    responsivePriority: 2,
-                    targets: -1
-                }
-            ],
-
-            autoWidth: false,
-            buttons: {
-                buttons: [
-                    {
-                        extend:    'copyHtml5',
-                        text:      '<i class="fa fa-copy"></i>',
-                        className: 'btn  btn-secondary mb-2',
-                        titleAttr: 'Copiar',
-                        exportOptions: {
-                            columns: [0, 1, 2,3]
-                        }
-                    },
-                    {
-                        extend: 'excelHtml5',
-                        text: '<i class="fas fa-file-excel"></i>',
-                        titleAttr: 'Exportar a Excel',
-                        className: 'btn  btn-success mb-2',
-                        exportOptions: {
-                            columns: [0, 1, 2,3]
-                        }
-                    },
-                    {
-                        extend:    'csvHtml5',
-                        text:      '<i class="fa fa-file-csv"></i>',
-                        titleAttr: 'Exportar a CSV',
-                        className: 'btn  btn-info mb-2',
-                        exportOptions: {
-                            columns: [0, 1, 2,3]
-                        }
-                    },
-                    {
-                        extend: 'pdfHtml5',
-                        text: '<i class="fas fa-file-pdf"></i>',
-                        titleAttr: 'Exportar a PDF',
-                        className: 'btn btn-danger mb-2',
-                        exportOptions: {
-                            columns: [0, 1, 2, 3]
-                        }
-                    },
-                    {
-                        extend: 'print',
-                        text: '<i style="color:white" class="fas fa-print"></i>',
-                        titleAttr: 'Imprimir',
-                        className: 'btn btn-warning mb-2',
-                        exportOptions: {
-                            columns: [0, 1, 2, 3]
-                        }
-                    }
-                ]
-            }
-        });
+            let export_columns = [0, 1, 2, 3];
     </script>
+
+    <script src="{{asset('js/pages/tables_datatables.js')}}"></script>
 
 
     <script>
