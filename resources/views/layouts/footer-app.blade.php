02feb96b87e2f1add3a85ccb6e3578b57ba51a6e
fvalentin1 <71657263+fvalentin1@users.noreply.github.com>
2022-07-13 16:20:24 -0400

    add user faq view

--- resources/views/layouts/footer-app.blade.php
+++ resources/views/layouts/footer-app.blade.php
@@ -17,7 +17,7 @@
                 <div class="col-sm-6 pb-1">
                     <div class="card border-0 bg-dark text-info">
                         <div class="card-body">
-                            <a class="text-white" href="https://www.google.com/maps/@-36.8264254,-73.0528476,20z"><h5 class="card-title"><i class="fa-solid fa-location-dot pe-2"></i>Tienda Física</h5></a>
+                            <a class="text-white" href="https://www.google.com/maps/place/Freire+522,+Concepci%C3%B3n,+B%C3%ADo+B%C3%ADo/@-36.826321,-73.052813,20z/data=!4m5!3m4!1s0x9669b5da9927f84f:0x716bb9842b0c12be!8m2!3d-36.8263347!4d-73.0528168?hl=es"><h5 class="card-title"><i class="fa-solid fa-location-dot pe-2"></i>Tienda Física</h5></a>
                         <p class="card-text">
                             Nos encontramos en Freire #522, Galería Caracol, 4to piso, Local 185. Concepción.
                             <br>
@@ -32,7 +32,7 @@
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6 text-center pt-5">
                     <p class="text-white">Accede a nuestras preguntas frecuentes</p>
-                    <a href="/soporte" type="button" class="btn btn-outline-success {{ request()->is('soporte') ? 'disabled' : '' }}">Preguntas Frecuentes</a>
+                    <a href="/preguntas-frecuentes" type="button" class="btn btn-outline-success {{ request()->is('soporte') ? 'disabled' : '' }}">Preguntas Frecuentes</a>
                 </div>
             </div>
         </div>
