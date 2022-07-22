db4910b241e4e4833089a163ba4c014e25b040c9
fvalentin1 <71657263+fvalentin1@users.noreply.github.com>
2022-07-22 19:09:24 -0400

    Update filter-products.blade.php

--- resources/views/livewire/filter-products.blade.php
+++ resources/views/livewire/filter-products.blade.php
@@ -1,6 +1,6 @@
 <div class="mx-5">
     <div class=" row my-4">
-        <div class="col-lg-3 col-md-5 col-sm-12 mt-3 bg-success">
+        <div class="col-lg-3 col-md-5 col-sm-12 mt-3">
                     <label for="">Categorías: </label>
                     <select wire:model="category_id" wire:change="filter" class="form-control form-select"
                         data-placeholder="Selecciona una categoría">
@@ -12,7 +12,7 @@
 
         </div>
 
-        <div class="col-lg-3 col-md-5 col-sm-12 mt-3 align-items-end bg-warning">
+        <div class="col-lg-3 col-md-5 col-sm-12 mt-3 align-items-end">
             <label for="">Ordenar por:</label>
             <select wire:model="sort_by" wire:change="filter"  class="form-control form-select"
                 data-placeholder="Selecciona una categoría">
