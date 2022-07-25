b698d36f8d69ec63eac2e883111f272af1e59def
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-07-25 00:39:48 -0400

    navbar

--- resources/views/livewire/cart-shopping.blade.php
+++ resources/views/livewire/cart-shopping.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3 d-lg-none d-xl-block">
+    <div class="col-12 col-lg-auto mb-3 mb-lg-0 ms-2 mt-2 me-lg-3 d-lg-none d-xl-block ">
         <a href="{{route('cart.list')}}" class="btn btn-alt-secondary position-relative">
             <i style="color: #fff" class="fa-solid fa-cart-shopping"></i>
             <span class="badge position-absolute top-0 start-0 translate-middle bg-warning">{{$count}}</span>
